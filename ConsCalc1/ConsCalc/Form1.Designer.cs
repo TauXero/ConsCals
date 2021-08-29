d5bdfbb58d4cda80bf0b5e639997f529d9f51f82
TauXero <62438182+TauXero@users.noreply.github.com>
2021-08-29 13:38:58 +0900

    v0.1
    v0.1

---
+++ ConsCalc1/ConsCalc/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ConsCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(187, 32);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(187, 84);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "착공(착수)일";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "준공(완료)예정일";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "공사(용역) 중지일";
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(187, 139);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "공사(용역) 중지해제일";
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Location = new System.Drawing.Point(187, 190);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(442, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "---";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(442, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "---";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(442, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "---";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(442, 199);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "---";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "계산";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label9.Location = new System.Drawing.Point(730, 289);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 11);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "v0.1 KiDragon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 310);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "중지해제 계산기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ ConsCalc1/ConsCalc/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan stopdays,remainWork, oneday;
+            
+            //착공일, 준공일, 중지일, 중지해제일  컨트롤값
+            string startwork = dateTimePicker1.Value.ToShortDateString();
+            string endwork = dateTimePicker2.Value.ToShortDateString();
+            string stopwork = dateTimePicker3.Value.ToShortDateString();
+            string restartwork = dateTimePicker4.Value.ToShortDateString();
+
+            //착공일, 준공일, 중지일, 중지해제일, 변경준공예정일    날짜
+            DateTime StartWork = Convert.ToDateTime(startwork);
+            DateTime EndWork = Convert.ToDateTime(endwork);
+            DateTime StopWork = Convert.ToDateTime(stopwork);
+            DateTime RestartWork = Convert.ToDateTime(restartwork);
+            DateTime newEndWork;
+            
+            stopdays = RestartWork - StopWork;     //중지일수
+            newEndWork = EndWork + stopdays;  //변경준공예정일
+            remainWork = EndWork - StopWork;    //잔여일수
+            oneday = new TimeSpan(1, 0, 0, 0);
+
+            if (stopdays.Days <= 0)
+            {
+                MessageBox.Show("오류 : 중지기간이 0이하");
+                return;
+            }
+
+            if (remainWork.Days <= 0)
+            {
+                MessageBox.Show("오류 : 잔여기간이 0이하");
+                return;
+            }
+
+            //기간, 수행, 잔여, 변경 결과출력
+            label5.Text = "공사(용역)기간 : " + StartWork.ToShortDateString() + " ~ " 
+                + EndWork.ToShortDateString();
+            label6.Text = "중지기간 : " + StopWork.ToShortDateString() + " ~ " 
+                + (RestartWork - oneday).ToShortDateString() 
+                + "(중지일 : " + stopdays.Days.ToString() + "일)";
+            label7.Text = "잔여공기 : " + (remainWork.Days+1).ToString() + "일";
+            label8.Text = "변경공사(용역)기간 : " + StartWork.ToShortDateString() + " ~ "
+                 + (newEndWork.ToShortDateString());            
+        }
+    }
+}
